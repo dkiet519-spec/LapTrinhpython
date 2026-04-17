2366978323c2e1bca81b0cd7724ea069915013a9
dkiet519-spec <dkiet519@gmail.com>
2026-04-17 09:35:56 +0700

    bai tap

---
+++ BT_VD.py
@@ -0,0 +1,9 @@
+import math
+a = float(input("Nhap chieu dai: "))
+b = float(input("Nhap chieu rong: "))
+c = float(input("Nhap chieu cao: "))
+Schunhat = a*b
+Vchunhat = Schunhat*c# tính thể tích hình chữ nhật
+print ("Dien tich hinh chu nhat = ",Schunhat)
+print ("The tich hinh chu nhat = ",Vchunhat)
+
