5083bc762756077bfe5299a0428aa5a7bc46dc88
dkiet519-spec <dkiet519@gmail.com>
2026-04-21 10:37:27 +0700

    thêm

--- baitapchuoi2.py
+++ baitapchuoi2.py
@@ -2,4 +2,7 @@ s='''     Quê hương
      Quê hương là   chùm khế   ngọt.
          Cho con trèo hái   mỗi  ngày.   
      Quê  hương là   đường  đi học .
-     '''
+       con về  rợp bướm  vàng bay .
+       Đỗ     Trung Quân   '''
+print (s.strip())
+print (s.strip().replace("   ", " ").replace("  ", " ").replace("  "," "))
