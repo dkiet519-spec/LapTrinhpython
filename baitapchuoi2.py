5f4f203a64d461d6b21f3dd3c22e836feabd9802
dkiet519-spec <dkiet519@gmail.com>
2026-04-21 09:30:26 +0700

    chuoi

---
+++ baitapchuoi2.py
@@ -0,0 +1,5 @@
+s='''     Quê hương
+     Quê hương là   chùm khế   ngọt.
+         Cho con trèo hái   mỗi  ngày.   
+     Quê  hương là   đường  đi học .
+     '''
---
+++ chuoi.py
@@ -0,0 +1,7 @@
+s =''' Chiều chiều trước trước bến Văn Lậu
+       Ai ngồi, ai câu, ai sầu, ai thảm 
+       Ai thương, ai cảm, ai nhớ, ai trông
+       Thuyền ai thấp thoáng bên ven sông
+       Đưa câu mái vẩy chạnh lòng nước non'''
+print ("so lan ai tim thay", s.count("ai", 0, len(s)))
+         
