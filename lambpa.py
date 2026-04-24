cfe1130cee4cdb6b679ee1f8b76e4834f43f7f35
dkiet519-spec <dkiet519@gmail.com>
2026-04-24 10:26:40 +0700

    thêm c

--- lambpa.py
+++ lambpa.py
@@ -1,11 +1,18 @@
 
-def cong(b) :
-    return b +15
+# def cong(b) :
+#     return b +15
 
-b = int(input("nhap so: "))
-print (cong(b))    
+# b = int(input("nhap so: "))
+# print (cong(b))    
 
-def show(a):
-    return a 
-a = input("nhap chuoi: ")
-print(show(a))
+# def show(a):
+#     return a 
+# a = input("nhap chuoi: ")
+# print(show(a))
+
+def nhan(x,y):
+    return x * y
+x = int(input("nhap so x: "))
+y = int(input("nhap so y: "))
+print(nhan(x,y))
+   
