565152a7ae76ff8510c8a360e1a9532bc8689c3b
dkiet519-spec <dkiet519@gmail.com>
2026-04-24 10:17:01 +0700

    sửa lại a và b

--- lambpa.py
+++ lambpa.py
@@ -1,9 +1,11 @@
-def show(a) :
-    a = int(input("nhap so: "))
-    inra=a
-    show(inra)
-    
+
 def cong(b) :
     return b +15
+
 b = int(input("nhap so: "))
 print (cong(b))    
+
+def show(a):
+    return a 
+a = input("nhap chuoi: ")
+print(show(a))
