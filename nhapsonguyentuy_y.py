ef6a6d520ec99dd866b72f202531b28a29b47fe7
dkiet519-spec <dkiet519@gmail.com>
2026-05-11 16:54:52 +0700

    sửa thêm cho bài

--- nhapsonguyentuy_y.py
+++ nhapsonguyentuy_y.py
@@ -20,3 +20,14 @@ lonnhat = max(dayso) #tìm số lớn nhất trong dãy số
 print("Số lớn nhất là: ", lonnhat)
 nhonhat = min(dayso) #tìm số nhỏ nhất trong dãy số
 print("Số nhỏ nhất là: ", nhonhat)
+
+def kiemtra(dayso):
+    for i in range(len(dayso)-1):
+        if dayso[i] > dayso[i+1]: #kiểm tra nếu phần tử hiện tại lớn hơn phần tử tiếp theo thì dãy số không tăng dần
+            return False
+    return True
+if kiemtra(dayso):
+    print("Dãy số đã nhập là dãy số tăng dần.", dayso)
+else:
+    print("Dãy số đã nhập không phải là dãy số tăng dần.", dayso)
+
