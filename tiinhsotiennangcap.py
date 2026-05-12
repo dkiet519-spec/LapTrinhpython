aab530600befbc3620251be0481cbda6c7cab5a6
dkiet519-spec <dkiet519@gmail.com>
2026-05-12 11:18:53 +0700

    chức năng đổi tiền bài 20

---
+++ tiinhsotiennangcap.py
@@ -0,0 +1,19 @@
+menhgia = [1,2,5,10,20,50,100,200,500]
+def demtien(menhgia, tien):# def dùng trong trường hợp bên ngoài code muốn gọi lại
+    print(f"----------------SỐ TIỀN {tien}được đổi thành----------")
+    tong  = 0
+    for i in reversed(menhgia):
+        sotien = tien // i
+        tien = tien % i
+        tong += sotien
+        if sotien > 0: 
+         print (f"số tờ {i} là: {sotien}")#f giúp đưa dữ liệu vào cho đẹp không cần phải phẩy
+    print("tổng số tờ cần đổi:", tong)    
+    return tong
+
+tien = int(input("Nhập số tiền: "))
+print("Mệnh giá có sẵn:", menhgia)
+demtien(menhgia, tien)
+
+
+
