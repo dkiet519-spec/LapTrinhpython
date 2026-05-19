60b4c00963a20d7bae59f39f303c6cadd1ab43ba
dkiet519-spec <dkiet519@gmail.com>
2026-05-19 15:42:58 +0700

    bài tập 121

---
+++ strobogrammatic_2-10.py
@@ -0,0 +1,48 @@
+# def la_so_strobogrammatic(n):
+#     # if n == 0:
+#     #     return True
+#     # if n == 1:
+#     #     return n in strobogrammatic
+#     chuoi_n = str(n)
+#     left, right = 0, len(chuoi_n) - 1
+#     while left <= right:
+#         if chuoi_n[left] not in strobogrammatic or chuoi_n[right] not in strobogrammatic:
+#             return False
+#         if strobogrammatic[chuoi_n[left]] != chuoi_n[right]:
+#             return False
+#         left += 1
+#         right -= 1
+#     return True
+# strobogrammatic = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}
+
+# #strobogrammatic nằm trong khoảng từ 2 đến 9 
+# for i in range(2, 10):
+#     if la_so_strobogrammatic(i):
+#         print(f"\n{i}")
+
+#--------------------------------------------------
+def la_so_strobogrammatic(n):
+    # if n == 0:
+    #     return True
+    # if n == 1:
+    #     return n in strobogrammatic
+    chuoi_n = str(n)
+    left, right = 0, len(chuoi_n) - 1
+    while left <= right:
+        if chuoi_n[left] not in strobogrammatic or chuoi_n[right] not in strobogrammatic:
+            return False
+        if strobogrammatic[chuoi_n[left]] != chuoi_n[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+strobogrammatic = {"0": "0", "1": "1", "2": "2","5": "5", "6": "9", "8": "8", "9": "6"}
+
+#strobogrammatic nằm trong khoảng từ 2 đến 9 mở rộng thêm 2 và 5
+for i in range(2, 10):
+    if la_so_strobogrammatic(i):
+        print(f"\n{i}")
+
+
+        
+        
